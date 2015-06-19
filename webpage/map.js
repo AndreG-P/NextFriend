bc9838e1978262acc59f9cbe776551f3e2517a0e
Neurosonic <wittstock@campus.tu-berlin.de>
2015-06-19 14:20:15 +0200

    v0.0.2

--- webpage/map.js
+++ webpage/map.js
@@ -15,27 +15,73 @@ function moveMapToPoint(map, lat, lng, zoom ){
   map.setZoom(zoom);
 }
 
+<<<<<<< HEAD
+function positionError(){
+	alert("Error by geting position");
+}
+
+function foundUserLocation(location){		
+	alert("location: "+ location.coords);
+}
+
+=======
 /*
+>>>>>>> c7962fee60df088f00a45010038ee960efe77215
 function determineLocation(){
+	//var foundUserLocation, positionError;
 	if(navigator.geolocation){
-	//	navigator.geolocation.getCurrentPosition(foundUserLocation, positionError);
-		alert("determinelocation: "+ navigator.geolocation);
+		navigator.geolocation.getCurrentPosition(foundUserLocation, positionError);
+		
 	}else{
 		alert("Error in determinelocation");
 	}
 }
 
-function foundUserLocation(location){
-	location.coords;
 
+<<<<<<< HEAD
+
+function addMarkersToMap(map, lat, lng) {
+	  var newMarker = new H.map.Marker({lat:+lat, lng:+lng});
+	  map.addObject(newMarker);  
+  }
+=======
 	
 }
 */
 
+>>>>>>> c7962fee60df088f00a45010038ee960efe77215
 /**
  * Boilerplate map initialization code starts below:
  */
+function map_ini(){
+	//Step 1: initialize communication with the platform
+	var platform = new H.service.Platform({
+	  app_id: 'eAj5kHCKYcWnfIcHfPJf',
+	  app_code: 'belhuu93pAdVRzaRV_nI0A',
+	  useCIT: true,
+	  useHTTPS: true
+	});
+	var defaultLayers = platform.createDefaultLayers();
 
+	//Step 2: initialize a map  - not specificing a location will give a whole world view.
+	var map = new H.Map(document.getElementById('map'),
+	  defaultLayers.normal.map);
+
+	//Step 3: make the map interactive
+	// MapEvents enables the event system
+	// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+	var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+	// Create the default UI components
+	var ui = H.ui.UI.createDefault(map, defaultLayers);
+
+<<<<<<< HEAD
+	// Now use the map as required...
+	//moveMapToBerlin(map);
+	determineLocation();
+	moveMapToPoint(map, 52.53, 13.39, 14 ); // todo geting user location
+}
+=======
 //Step 1: initialize communication with the platform
 var platform = new H.service.Platform({
   app_id: 'eAj5kHCKYcWnfIcHfPJf',
@@ -61,3 +107,4 @@ var ui = H.ui.UI.createDefault(map, defaultLayers);
 //moveMapToBerlin(map);
 //determineLocation();
 moveMapToPoint(map, 52.53, 13.39, 14 );
+>>>>>>> c7962fee60df088f00a45010038ee960efe77215
